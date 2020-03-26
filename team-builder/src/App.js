96c9777c469bdb33a0071bb450a15aa1d7486c62
Enclosed Letter <C.LuisMartinez.A@gmail.com>
2020-03-26 01:50:12 -0700

    done with key error

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Form from "./Form"; //importing Form
 
 function App() {
-  const [teamMember, setTeamMember] = useState("");
+  const [teamMember, setTeamMember] = useState([]);
   const addMember =(newMember) => { //what does this function do
     setTeamMember(
       [...teamMember, newMember] // we are setting the setTeamMember setter to say spread up the teamMember array and get the newMember?
@@ -13,6 +13,15 @@ function App() {
   return (
     <div className="App">
       <Form addMember= {addMember}/> {/*rendering the Form component, pass the function yo ujust made as a prop to Form. the "addMember" is what we are naming this, so when we go inside of our form we will see the prop named this, then {addMember} will run the function*/}
+      {teamMember.map((item, index)=>{
+        return (
+          <div key= {index}/*this is doing it so it gets rid of the "you need a key" error*/ > 
+            <h2>{item.name}</h2>
+            <h3>{item.email}</h3>
+            <p>{item.role}</p>
+          </div>
+        )
+      })}
     </div>
   );
 }
